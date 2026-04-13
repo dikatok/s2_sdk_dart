b8ae000029ce0208d78848bd25e2cde4727c925c
Andika Tanuwijaya <holy.arc.64633@gmail.com>
2026-04-13 19:59:45 +0700

    modify signature for constructing append record

--- lib/src/record.dart
+++ lib/src/record.dart
@@ -6,18 +6,32 @@ class S2AppendRecord {
   final List<(Uint8List, Uint8List)>? headers;
   final int? timestamp;
 
-  S2AppendRecord._({
+  S2AppendRecord({
     required this.body,
     required this.headers,
     required this.timestamp,
   });
+
+  factory S2AppendRecord.fromString(
+    String body, {
+    Map<String, dynamic>? headers,
+    int? timestamp,
+  }) {
+    return S2AppendRecord(
+      body: utf8.encode(body),
+      headers: headers?.entries
+          .map((e) => (utf8.encode(e.key), utf8.encode(e.value)))
+          .toList(),
+      timestamp: timestamp,
+    );
+  }
 
   factory S2AppendRecord.fromObject(
     Map<String, dynamic> body, {
     Map<String, dynamic>? headers,
     int? timestamp,
   }) {
-    return S2AppendRecord._(
+    return S2AppendRecord(
       body: utf8.encode(jsonEncode(body)),
       headers: headers?.entries
           .map((e) => (utf8.encode(e.key), utf8.encode(e.value)))
